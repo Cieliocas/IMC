08544ebafe8561a3cd286f4ab2efecead181be26
Cieliocas <francielioevangelista@hotmail.com>
2025-01-15 00:20:35 -0300

    import utility and component modules in App component; add styles for better layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,8 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import { calculateIMC } from './utils/calculateIMC';
+import { Form } from './components/Form/Form';
+import { Result } from './components/Result/Result';
+import styles from './App.module.css';
 
 function App() {
   const [count, setCount] = useState(0)
